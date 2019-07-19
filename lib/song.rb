a964df521dd4936ec7b6974300c69d37466965d3
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-07-19 06:50:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Song
   extend Memorable::ClassMethods, Findable
-  include Menorable::InstanceMethods, Paramable
+  include Memorable::InstanceMethods, Paramable
   
   attr_accessor :name
   attr_reader :artist
